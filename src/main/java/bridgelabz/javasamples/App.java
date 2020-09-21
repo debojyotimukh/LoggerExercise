a2420e00750348774a9c09185633046298f63b07
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-09-21 16:26:40 +0530

    source files added, .gitignore modified

---
+++ src/main/java/bridgelabz/javasamples/App.java
@@ -0,0 +1,23 @@
+package bridgelabz.javasamples;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	private static final Logger LOG = LogManager.getLogger(App.class);
+
+	public static void main(String[] args) {
+		String message = "Hello, World ";
+		LOG.debug(message + "will be printed on Debug");
+		LOG.info(message + "printed on info");
+		LOG.warn(message + "printed on waarn");
+		LOG.error(message + "printed on error");
+		LOG.fatal(message + "printed on fatal");
+		LOG.info("Appending string: {}.", message);
+		System.out.println(message);
+	}
+}
